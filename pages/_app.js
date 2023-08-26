041a3db2df8c7ddf420ada58f4d73441cc952c99
LaThanhTrong <lathanhtrong322002@gmail.com>
2023-08-26 09:29:17 +0700

    add progress-bar

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps:{session, ...pageProps} }) {
   return (
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps:{session, ...pageProps} }) {
           <CartContextProvider>
             <Header></Header>
               <div id="container">
+                <NextNProgress color='linear-gradient(90deg, #b656cb, #10a1a0)' startPosition={0.3} stopDelayMs={200} height={5} />
                 <Component {...pageProps} />
               </div>
             <Footer></Footer>
