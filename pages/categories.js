3ef2080a3cb2621c7c72147248dcb26c40a99eab
LaThanhTrong <lathanhtrong322002@gmail.com>
2023-11-16 20:47:39 +0700

    fix

--- pages/categories.js
+++ pages/categories.js
@@ -1,7 +1,6 @@
 import Center from "@/components/Center";
 import { Category } from "@/models/Category";
 import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/Product";
 import { Inventory } from "@/models/Inventory";
 import ProductBox from "@/components/ProductBox";
 import Link from "next/link";
@@ -10,24 +9,25 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]";
 import { useState, useEffect } from "react";
 import Carousel from "react-multi-carousel";
+import { Product } from "@/models/Product";
 import "react-multi-carousel/lib/styles.css";
 
-export default function CategoriesPage({mainCategories, categoriesProducts, wishedProducts=[], categ}){
+export default function CategoriesPage({ mainCategories, categoriesProducts, wishedProducts = [], categ }) {
     const [selected, setSelected] = useState([])
     const [categoriesSelected, setCategoriesSelected] = useState(categ)
     const [expandedCategories, setExpandedCategories] = useState({});
     const toggleCategory = (id) => {
         setExpandedCategories((prevState) => ({
-          ...prevState,
-          [id]: !prevState[id],
+            ...prevState,
+            [id]: !prevState[id],
         }));
     };
     const [data, setData] = useState(() => {
         let item = {}
-        for(let i = 0; i<mainCategories.length; i++){
+        for (let i = 0; i < mainCategories.length; i++) {
             item[mainCategories[i]._id] = []
-            for(let j = 0; j<mainCategories.length; j++){
-                if(mainCategories[j].parent?.toString() === mainCategories[i]._id.toString()){
+            for (let j = 0; j < mainCategories.length; j++) {
+                if (mainCategories[j].parent?.toString() === mainCategories[i]._id.toString()) {
                     item[mainCategories[i]._id].push(mainCategories[j])
                 }
             }
@@ -37,88 +37,88 @@ export default function CategoriesPage({mainCategories, categoriesProducts, wish
 
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5,
-          slidesToSlide: 5,
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5,
+            slidesToSlide: 5,
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 4,
-          slidesToSlide: 4,
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4,
+            slidesToSlide: 4,
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 3,
-          slidesToSlide: 3,
+            breakpoint: { max: 1024, min: 464 },
+            items: 3,
+            slidesToSlide: 3,
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 2,
-          slidesToSlide: 2,
+            breakpoint: { max: 464, min: 0 },
+            items: 2,
+            slidesToSlide: 2,
         }
     };
-    
-    function handleChange(e){
-        if(e.target.checked){
+
+    function handleChange(e) {
+        if (e.target.checked) {
             setSelected([...selected, e.target.value])
         }
-        else{
+        else {
             setSelected(selected.filter(s => s !== e.target.value))
             document.querySelectorAll(`.render-${e.target.value}`).forEach(el => {
                 el.checked = false
             })
         }
     }
-    
-    useEffect(()=>{
-        if(selected.length > 0){
+
+    useEffect(() => {
+        if (selected.length > 0) {
             setCategoriesSelected(categ.filter(c => selected.find(s => s === c.name)))
         }
-        else{
+        else {
             setCategoriesSelected(categ)
         }
-    },[selected])
+    }, [selected])
 
-    const renderCategories = (categories, level=0) => {
+    const renderCategories = (categories, level = 0) => {
         return (
-          <div style={{ marginLeft: `${level * 20}px` }}>
-            {categories.map((category, i) => {
-              const id = `${category.name}-${level}-${i}`; // Generate a unique id
-              return (
-                <div key={id} className="flex flex-col items-start my-2">
-                  <div className="flex items-center">
-                    <input
-                      className={"hidden render-"+category.name}
-                      id={`check-${id}`} // Use the unique id for each checkbox
-                      type="checkbox"
-                      value={category.name}
-                      onChange={ev => handleChange(ev)}
-                    />
-                    <label className="flex items-center" htmlFor={`check-${id}`}>
-                      <span className="w-5 h-5 inline-block mr-2 rounded border border-gray-400 flex items-center justify-center">
-                        <svg className="hidden w-3 h-3 text-blue-500 pointer-events-none" viewBox="0 0 173.867 173.867">
-                          <path d="M57.253,166.012L0,108.758l34.142-34.142l23.111,23.111l86.582-86.581l34.142,34.141L57.253,166.012z"/>
-                        </svg>
-                      </span>
-                      {category.name}
-                    </label>
-                    {data[category._id] && data[category._id].length > 0 && (
-                        <>
-                            <button onClick={() => toggleCategory(id)} className="ml-2 text-blue-500 underline">
-                                {expandedCategories[id] ? 'Collapse' : 'Expand'}
-                            </button>
-                        </>
-                    )}
-                  </div>
-                  {expandedCategories[id] && renderCategories(data[category._id], level+1)}
-                </div>
-              );
-            })}
-          </div>
+            <div style={{ marginLeft: `${level * 20}px` }}>
+                {categories.map((category, i) => {
+                    const id = `${category.name}-${level}-${i}`; // Generate a unique id
+                    return (
+                        <div key={id} className="flex flex-col items-start my-2">
+                            <div className="flex items-center">
+                                <input
+                                    className={"hidden render-" + category.name}
+                                    id={`check-${id}`} // Use the unique id for each checkbox
+                                    type="checkbox"
+                                    value={category.name}
+                                    onChange={ev => handleChange(ev)}
+                                />
+                                <label className="flex items-center" htmlFor={`check-${id}`}>
+                                    <span className="w-5 h-5 inline-block mr-2 rounded border border-gray-400 flex items-center justify-center">
+                                        <svg className="hidden w-3 h-3 text-blue-500 pointer-events-none" viewBox="0 0 173.867 173.867">
+                                            <path d="M57.253,166.012L0,108.758l34.142-34.142l23.111,23.111l86.582-86.581l34.142,34.141L57.253,166.012z" />
+                                        </svg>
+                                    </span>
+                                    {category.name}
+                                </label>
+                                {data[category._id] && data[category._id].length > 0 && (
+                                    <>
+                                        <button onClick={() => toggleCategory(id)} className="ml-2 text-blue-500 underline">
+                                            {expandedCategories[id] ? 'Collapse' : 'Expand'}
+                                        </button>
+                                    </>
+                                )}
+                            </div>
+                            {expandedCategories[id] && renderCategories(data[category._id], level + 1)}
+                        </div>
+                    );
+                })}
+            </div>
         );
     };
-    
+
     return (
         <>
             <Center>
@@ -127,7 +127,7 @@ export default function CategoriesPage({mainCategories, categoriesProducts, wish
                         <div id="menu" className="max-w-fit">
                             <button id="burger-button">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                                 </svg>
                             </button>
                             <div id="categories">
@@ -135,20 +135,20 @@ export default function CategoriesPage({mainCategories, categoriesProducts, wish
                             </div>
                         </div>
                         <h2 className="font-bold text-[1.5rem] mb-4 text-center">Filter categories</h2>
-                    </div> 
+                    </div>
                 </div>
-   
+
                 {categoriesSelected.map(category => (
                     <div key={category._id} className="mb-10">
                         <div className="mt-8 pt-7 mb-3 font-bold text-[1.5rem]">
                             {category.name}
                             <span className="font-normal text-[1.1rem] ml-4">
-                                <Link className="underline text-[#555]" href={'/category/'+category._id}>Show all {category.name}</Link>
+                                <Link className="underline text-[#555]" href={'/category/' + category._id}>Show all {category.name}</Link>
                             </span>
                         </div>
                         <div className="z-0">
                             <Carousel responsive={responsive}>
-                                {categoriesProducts[category._id] && categoriesProducts[category._id].map((i,index) => (
+                                {categoriesProducts[category._id] && categoriesProducts[category._id].map((i, index) => (
                                     <div key={index} className="border-2 border-[#edeaea] rounded-md mr-7">
                                         <ProductBox key={i.product._id} {...i.product} inventoryId={i._id} quantity={i.quantity} price={i.price} wished={wishedProducts.includes(i._id)} categ={categ}></ProductBox>
                                     </div>
@@ -175,18 +175,18 @@ async function findAllChildCategoryIds(categoryId) {
     return childCatIds;
 }
 
-export async function getServerSideProps(ctx){
+export async function getServerSideProps(ctx) {
     await mongooseConnect()
     const categories = await Category.find()
     const categ = await Category.find().populate('parent')
     const mainCategories = categories
     const categoriesProducts = {}
     const allFetchedProductsId = [];
-    for(const mainCat of mainCategories){
+    for (const mainCat of mainCategories) {
         const mainCatId = mainCat._id.toString()
         const childCatIds = await findAllChildCategoryIds(mainCat._id);
         const categoriesIds = [mainCatId, ...childCatIds]
-        const products = await Product.find({category: categoriesIds}, null, {sort: {'_id':-1}})
+        const products = await Product.find({ category: categoriesIds }, null, { sort: { '_id': -1 } })
         const productIds = products.map(product => product._id);
         const inventory = await Inventory.find({ product: { $in: productIds } }).populate('product')
         allFetchedProductsId.push(...inventory.map(i => i._id.toString()))
@@ -196,7 +196,7 @@ export async function getServerSideProps(ctx){
     const wishedProducts = session?.user ? await WishedProduct.find({
         userEmail: session.user.email,
         inventory: allFetchedProductsId,
-      }) : []
+    }) : []
     return {
         props: {
             mainCategories: JSON.parse(JSON.stringify(mainCategories)),
